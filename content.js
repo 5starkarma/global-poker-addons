df0dbc74593c9757ecc1a2a8000cd295809108c2
David Alford <davidalford678@gmail.com>
2018-10-06 21:34:19 -0700

    Add files via upload

--- content.js
+++ content.js
@@ -7,45 +7,46 @@ var delay = ( function() {
         timer = setTimeout(callback, ms);
     };
 })();
-
+//set hotkeys. 81 is 'Q'. 68 is '
 document.onkeydown = function(e) {
     if (e.which == 81) {
-        const foldBtn1 = document.querySelector('.future-actions.fold')
+        const futureFoldBtn = document.querySelector('.future-actions.fold')
         const foldBtn = document.querySelector('.action-button.action-fold.show')
-            if (foldBtn1) foldBtn1.click();
+            if (futureFoldBtn) futureFoldBtn.click();
             else if (foldBtn) foldBtn.click();
     } else if (e.which == 68) {
-        const test1 = document.querySelector('.action-button.action-bet.show')
-        const test2 = document.querySelector('.action-button.action-raise.show')
-            if (test1) test1.click();
-            else if (test2) test2.click();
+        const betBtn = document.querySelector('.action-button.action-bet.show')
+        const raiseBtn = document.querySelector('.action-button.action-raise.show')
+            if (betBtn) betBtn.click();
+            else if (raiseBtn) raiseBtn.click();
     } else if (e.which == 69) {
-        const test3 = document.querySelector('.action-button.action-call.show')
-        const test4 = document.querySelector('.action-button.action-check.show')
-            if (test3) test3.click();
-            else if (test4) test4.click();
+        const callBtn = document.querySelector('.action-button.action-call.show')
+        const checkBtn = document.querySelector('.action-button.action-check.show')
+            if (callBtn) callBtn.click();
+            else if (checkBtn) checkBtn.click();
     } else if (e.which == 38) {
-        const test5 = document.querySelector('.bet-input')
-            if (test5) test5.focus().select();
+        const betInputBox = document.querySelector('.bet-input')
+            if (betInputBox) betInputBox.click();
     }
 };
 
 console.log("Hotkeys activated. Tiling and joining table...");
-
+//tile tables and join newly opened table
 delay(function(){
     document.querySelector('.tile-tables').click();
-    const span1 = document.querySelector('.action-join')
-        if (span1) span1.click();
+    const joinBtn = document.querySelector('.action-join')
+        if (joinBtn) joinBtn.click();
 }, 900 ); // end delay
 
 console.log("Adding mouseover active table styling..");
 
 //Add red color to bets when mouse is over table
 document.querySelector('.view-container').addEventListener("mouseover", mouseOver);
+document.querySelector('.view-container').addEventListener("mouseout", mouseOut);
+
 function mouseOver() {
     document.querySelector('.view-container').style.color = "white";
 }
-document.querySelector('.view-container').addEventListener("mouseout", mouseOut);
 function mouseOut() {
     document.querySelector('.view-container').style.color = "grey";
 }
